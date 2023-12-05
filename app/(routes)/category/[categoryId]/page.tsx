a9afa3195bdbd8a7ba2287575fe445a503df71ea
unknown <anhnd.yrc@gmail.com>
2023-12-05 10:39:27 +0700

    create category page

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,7 +1,40 @@
+import getCategory from "@/actions/get-category";
+import getColors from "@/actions/get-colors";
+import getProducts from "@/actions/get-products";
+import getSizes from "@/actions/get-sizes";
+import Billboard from "@/components/Billboard";
+import Container from "@/components/UI/Container";
 import React from "react";
 
-const CategoryPage = () => {
-  return <div>CategoryPage</div>;
+interface CategoryPageProps {
+  params: {
+    categoryId: string;
+  };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
+}
+
+const CategoryPage: React.FC<CategoryPageProps> = async ({
+  params,
+  searchParams,
+}) => {
+  const products = await getProducts({
+    categoryId: params.categoryId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
+  });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(params.categoryId);
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
