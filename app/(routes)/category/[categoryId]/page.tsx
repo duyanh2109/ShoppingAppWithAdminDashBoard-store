f7cfef4de8b180ee32cd2a823944e63afc9fc45e
unknown <anhnd.yrc@gmail.com>
2023-12-06 12:29:47 +0700

    update filter for mobile version

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -39,7 +39,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            <MobileFilter/>
+            <MobileFilter sizes={sizes} colors={colors}/>
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
--- components/UI/MobileFilter.tsx
+++ components/UI/MobileFilter.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Size, Color } from "@/type";
+import Button from "./Button";
+import { Plus, X } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+import IconButton from "./Icon-button";
+import Filter from "./Filter";
 
-const MobileFilter = () => {
-  return (
-    <div>MobileFilter</div>
-  )
+interface MobileFilterProps {
+  sizes: Size[];
+  colors: Color[];
 }
+const MobileFilter: React.FC<MobileFilterProps> = ({ sizes, colors }) => {
+  const [open, setOpen] = useState(false);
+  const onOpen = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
 
-export default MobileFilter
+  return (
+    <>
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
+        Filters
+        <Plus size={20} />
+      </Button>
+      <Dialog
+        open={open}
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={onClose}
+      >
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+        <div className="fixed inset-0 z-40 flex">
+          <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl ">
+            <div className="flex items-center justify-end px-4">
+              <IconButton onClick={onClose} icon={<X size={15} />} />
+            </div>
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default MobileFilter;
