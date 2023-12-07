094946d52984b1c0ffce1070344186fdbdd4af7d
unknown <anhnd.yrc@gmail.com>
2023-12-07 10:14:44 +0700

    change background image of billboard to using next Image component to see if it can improve performance

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { Billboard as BillboardType } from "@/type";
+import Image from "next/image";
 
 interface BillboardProps {
   data: BillboardType;
 }
 const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
-    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
-      <div
-        className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover object-cover object-center"
-        style={{ backgroundImage: `url(${data?.imageUrl})` }}
-      >
-        <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
-          <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data?.label}
-          </div>
+    <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden relative h-[400px] lg:h-[500px]">
+      <Image
+        sizes="100vw"
+        objectFit="cover"
+        alt=""
+        layout="fill"
+        className="rounded-xl  aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover object-cover object-center w-full "
+        src={data?.imageUrl}
+      ></Image>
+
+      <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8 relative z-1">
+        <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
+          {data?.label}
         </div>
       </div>
     </div>
