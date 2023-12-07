90e4a742376634171855ebd9dba2fc1aac9b9625
unknown <anhnd.yrc@gmail.com>
2023-12-07 12:44:41 +0700

    fix bug dont highlight current tab  when click on galery tab of product card

--- components/gallery/GalleryTab.tsx
+++ components/gallery/GalleryTab.tsx
@@ -9,19 +9,26 @@ interface GalleryTabProps {
 const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
   return (
     <Tab className="relative aspect-square cursor-pointer flex  items-center justify-center rounded-md bg-white">
-      {({ selected })=>{
-        return <div>
-        <span className="h-full absolute w-full aspect-square inset-0 overflow-hidden rounded-md">
-            <Image
-            fill
-            src={image.url}
-            alt=""
-            sizes="100vw"
-            className="object-cover object-center"
-            />
-        </span>
-        <span className={cn("absolute inset-0 rounded-md ring-offset-2",selected?"ring-black":"ring-transparent")}/>
-        </div>
+      {({ selected }) => {
+        return (
+          <div>
+            <span className="h-full absolute w-full aspect-square inset-0 overflow-hidden rounded-md">
+              <Image
+                fill
+                src={image.url}
+                alt=""
+                sizes="100vw"
+                className="object-cover object-center"
+              />
+            </span>
+            <span
+              className={cn(
+                "absolute inset-0 rounded-md ring-offset-2",
+                selected ? "ring-black" : "ring-transparent"
+              )}
+            ></span>
+          </div>
+        );
       }}
     </Tab>
   );
