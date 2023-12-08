a41840431f0d439f4154fe352d34af3fa4ab40fb
unknown <anhnd.yrc@gmail.com>
2023-12-08 13:33:19 +0700

    fix bug

--- app/(routes)/cart/components/Summary.tsx
+++ app/(routes)/cart/components/Summary.tsx
@@ -41,34 +41,35 @@ const Summary = () => {
       setLoading(false);
     }
   };
-  return;
-  {
-    loading ? (
-      <div className="mt-16 rounded-lg bg-gray-500 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-        <ClipLoader
-          loading={loading}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
-      </div>
-    ) : (
-      <div className="mt-16 rounded-lg bg-gray-500 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-        <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
-        <div className="mt-6 space-y-4 ">
-          <div className="flex items-center justify-between border-t border-gray-200 pt-4">
-            <div className="text-base font-medium text-gray-900">
-              Order Total
-            </div>
-            <Currency value={totalPrice} />
-          </div>
+  return (
+    <>
+      {loading ? (
+        <div className="mt-16 rounded-lg bg-gray-500 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+          <ClipLoader
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
         </div>
-        <Button onClick={onCheckout} className="w-full mt-6">
-          Check out
-        </Button>
-      </div>
-    );
-  }
+      ) : (
+        <div className="mt-16 rounded-lg bg-gray-500 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
+          <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
+          <div className="mt-6 space-y-4 ">
+            <div className="flex items-center justify-between border-t border-gray-200 pt-4">
+              <div className="text-base font-medium text-gray-900">
+                Order Total
+              </div>
+              <Currency value={totalPrice} />
+            </div>
+          </div>
+          <Button onClick={onCheckout} className="w-full mt-6">
+            Check out
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Summary;
