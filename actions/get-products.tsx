95f767670933f046e739ce5fce0f40349d425619
unknown <anhnd.yrc@gmail.com>
2023-12-11 11:27:22 +0700

    update revalidate to retrieve product change right away

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -9,15 +9,15 @@ interface Query {
 }
 const getProducts = async (query: Query): Promise<Product[]> => {
   const url = qs.stringifyUrl({
-    url:URL,
-    query:{
-      categoryId:query.categoryId,
-      colorId:query.colorId,
-      sizeId:query.sizeId,
-      isFeatured:query.isFeatured
-    }
+    url: URL,
+    query: {
+      categoryId: query.categoryId,
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      isFeatured: query.isFeatured,
+    },
   });
-  const res = await fetch(url, { next: { revalidate: 60 } });
+  const res = await fetch(url, { next: { revalidate: 0 } });
   return res.json();
 };
 export default getProducts;
